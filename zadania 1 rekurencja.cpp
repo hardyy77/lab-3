ee26a1a411e7ee90270e8380f16c389bdb9dd2cd
hardyy77 <92890843+hardyy77@users.noreply.github.com>
2022-07-06 15:06:48 +0200

    Add files via upload

---
+++ zadania 1 rekurencja.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+
+int silnia(int n)
+{
+	if (n == 0) return 1;
+	else return n * silnia(n - 1);
+}
+
+int main()
+{
+	int liczba;
+	cout << "Podaj liczbe: ";
+	cin >> liczba;
+	cout << liczba << "! = " << silnia(liczba) << endl;
+	return 0;
+}
